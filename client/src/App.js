00a675b0978cce9af66f55b8cdcac47f50832770
Darwin Garcia <rdgt07@gmail.com>
2022-02-27 16:04:41 -0500

    Merge pull request #6 from Trevor-D-Anderson/KaisBranch
    Tested Goal and User routes and refactored + fixed minor bugs

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Card from "./components/Card";
 import Nav from "./components/Nav";
 import SearchBar from "./components/SearchBar";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
     <div className="App flex flex-col items-center">
-      <Nav />
+      {/* <Nav />
       <SearchBar />
-      <Card />
+      <Card /> */}
+      <RegisterForm />
     </div>
   );
 }
